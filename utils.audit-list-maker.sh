0a23f164243e60dea843c0d574b6ec57f47ec506
damola adebayo <adebayo10k@gmail.com>
2021-07-13 00:51:57 +0100

    moves more shared functions into shared functions library

--- utils.audit-list-maker.sh
+++ utils.audit-list-maker.sh
@@ -161,10 +161,6 @@ function main
 
 
 	###############################################################################################
-	# FUNCTIONS CALLED ONLY IF THIS PROGRAM USES A CONFIGURATION FILE:	
-	###############################################################################################
-
-	###############################################################################################
 	# PROGRAM-SPECIFIC FUNCTION CALLS:	
 	###############################################################################################	
 	
@@ -209,26 +205,8 @@ function main
 
 ###############################################################################################
 ####  FUNCTION DECLARATIONS  
-###############################################################################################
+##################################################################################
 
-# quick check that number of program arguments is within the valid range
-function check_no_of_program_args()
-{
-	#echo && echo "Entered into function ${FUNCNAME[0]}" && echo
-	
-	# establish that number of parameters is valid
-	if [ $actual_no_of_program_parameters -lt $min_expected_no_of_program_parameters -o \
-	$actual_no_of_program_parameters -gt $max_expected_no_of_program_parameters  ]
-	then
-		msg="Incorrect number of command line arguments. Exiting now..."
-		lib10k_exit_with_error "$E_INCORRECT_NUMBER_OF_ARGS" "$msg"
-	fi
-	
-	#echo && echo "Leaving from function ${FUNCNAME[0]}" && echo
-
-}
-
-###############################################################################################
 # entry test to prevent running this program on an inappropriate host
 function entry_test()
 {
@@ -236,23 +214,6 @@ function entry_test()
 	:
 }
 
-##########################################################################################################
-function get_user_permission_to_proceed
-{
-	echo " press ENTER to continue, or q to quit program."
-    echo && sleep 1
-    read last_chance
-
-    case $last_chance in 
-	[qQ])	echo
-				echo "Goodbye! Exiting now..."
-				exit 0 #
-				;;
-	*) 		echo "You're IN...Welcome!" && echo && sleep 1
-		 		;;
-    esac 
-
-}
 ##########################################################################################################
 # 
 function cleanup_and_validate_program_arguments()
@@ -689,101 +650,6 @@ function test_for_ignore_subdir
 
 }
 
-##########################################################################################################
-# firstly, we test that the parameter we got is of the correct form for an absolute file | sanitised directory path 
-# if this test fails, there's no point doing anything further
-# 
-function test_file_path_valid_form
-{
-	#echo && echo "ENTERED INTO FUNCTION ${FUNCNAME[0]}" && echo
-
-	test_result=
-	test_file_fullpath=$1
-	
-	#echo "test_file_fullpath is set to: $test_file_fullpath"
-	#echo "test_subdir_fullpath is set to: $test_subdir_fullpath"
-
-	if [[ $test_file_fullpath =~ $ABS_FILEPATH_NO_TB_REGEX ]]
-	then
-		echo "THE FORM OF THE INCOMING PARAMETER IS OF A VALID ABSOLUTE FILE PATH"
-		test_result=0
-	else
-		echo "AN INCOMING PARAMETER WAS SET, BUT WAS NOT A MATCH FOR OUR KNOWN PATH FORM REGEX "$ABS_FILEPATH_NO_TB_REGEX"" && sleep 1 && echo
-		echo "Returning with a non-zero test result..."
-		test_result=1
-		return $E_UNEXPECTED_ARG_VALUE
-	fi 
-
-
-	#echo && echo "LEAVING FROM FUNCTION ${FUNCNAME[0]}" && echo
-
-	return "$test_result"
-}
-
-###############################################################################################
-# need to test for read access to file 
-# 
-function test_file_path_access
-{
-	#echo && echo "ENTERED INTO FUNCTION ${FUNCNAME[0]}" && echo
-
-	test_result=
-	test_file_fullpath=$1
-
-	#echo "test_file_fullpath is set to: $test_file_fullpath"
-
-	# test for expected file type (regular) and read permission
-	if [ -f "$test_file_fullpath" ] && [ -r "$test_file_fullpath" ]
-	then
-		# test file found and accessible
-		echo "Test file found to be readable" && echo
-		test_result=0
-	else
-		# -> return due to failure of any of the above tests:
-		test_result=1 # just because...
-		echo "Returning from function \"${FUNCNAME[0]}\" with test result code: $E_REQUIRED_FILE_NOT_FOUND"
-		return $E_REQUIRED_FILE_NOT_FOUND
-	fi
-
-	#echo && echo "LEAVING FROM FUNCTION ${FUNCNAME[0]}" && echo
-
-	return "$test_result"
-}
-###############################################################################################
-# need to test for access to the file holding directory
-# 
-function test_dir_path_access
-{
-	#echo && echo "ENTERED INTO FUNCTION ${FUNCNAME[0]}" && echo
-
-	test_result=
-	test_subdir_fullpath=$1
-
-	#echo "test_subdir_fullpath is set to: $test_subdir_fullpath"
-
-	if [ -d "$test_subdir_fullpath" ] && cd "$test_subdir_fullpath" 2>/dev/null
-	then
-		# directory file found and accessible
-		echo "directory "$test_subdir_fullpath" found and accessed ok" && echo
-		test_result=0
-	elif [ -d "$test_subdir_fullpath" ] ## 
-	then
-		# directory file found BUT NOT accessible CAN'T RECOVER FROM THIS
-		echo "directory "$test_subdir_fullpath" found, BUT NOT accessed ok" && echo
-		test_result=1
-		echo "Returning from function \"${FUNCNAME[0]}\" with test result code: $E_FILE_NOT_ACCESSIBLE"
-		return $E_FILE_NOT_ACCESSIBLE
-	else
-		# -> directory not found: THIS CAN BE RESOLVED BY CREATING THE DIRECTORY
-		test_result=1
-		echo "Returning from function \"${FUNCNAME[0]}\" with test result code: $E_REQUIRED_FILE_NOT_FOUND"
-		return $E_REQUIRED_FILE_NOT_FOUND
-	fi
-
-	#echo && echo "LEAVING FROM FUNCTION ${FUNCNAME[0]}" && echo
-
-	return "$test_result"
-}
 ###############################################################################################
 
 main "$@"; exit
